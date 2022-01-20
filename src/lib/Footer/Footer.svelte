3bd0b4a16dca61742da4b9b7b29005cbe320654a
roidsaja <roid0520@gmail.com>
2022-01-20 17:36:51 +0300

    style: header and footer styling with sass

---
+++ src/lib/Footer/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+	<span>Created by Roid using SvelteKit</span>
+</footer>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap');
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 0 auto;
+
+		& > span {
+			font-family: Poppins, sans-serif;
+			font-style: normal;
+			font-size: 12px;
+			line-height: 36px;
+			color: #282538;
+		}
+	}
+</style>
---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,38 @@
+<header>
+	<a href="/">GitHub</a>
+	<a href="/">JobHunt</a>
+</header>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap');
+
+	header {
+		font-family: Poppins, sans-serif;
+		font-style: normal;
+		font-size: 24px;
+		line-height: 36px;
+		color: #282538;
+
+		width: 83%;
+		margin: 0 auto;
+		& > a:nth-child(1) {
+			font-weight: bold;
+			margin-right: 0.3rem;
+			cursor: pointer;
+			color: inherit;
+			text-decoration: none;
+		}
+		& > a:nth-child(2) {
+			font-weight: normal;
+			cursor: pointer;
+			color: inherit;
+			text-decoration: none;
+		}
+	}
+
+	@media screen and (max-width: 860px) {
+		header {
+			width: 90%;
+		}
+	}
+</style>
